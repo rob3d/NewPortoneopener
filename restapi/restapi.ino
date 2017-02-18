c890bea8a52c26383e03b71f42c0436fb12856d6
Roberto Santo <santoroberto00@gmail.com>
2017-02-18 21:48:37 +0100

    the new restapi
    in this version of restapi the software adds to the path its chip id.

--- restapi/restapi.ino
+++ restapi/restapi.ino
@@ -8,18 +8,18 @@ WiFiManager wifiManager;
 ESP8266WebServer server(80);
 #include "RestClient.h"
 String shouldOpen ="SHOULD_OPEN";
-const char* path = "/api/smart_gates/1/status";
 
 //RestClient client = RestClient("arduino-http-lib-test.herokuapp.com");
 RestClient client = RestClient("my.milan.maison",443, "A9 E5 6D E5 9F 55 3E F4 1C F7 1A BC 17 70 24 83 68 26 52 18");
 int mosfet = 12;
 
-void setup() {
+ const char* path = "/api/smart_gates/"+ESP.getChipId();
+ 
+void setup() {  
   pinMode(mosfet, OUTPUT);
   digitalWrite(mosfet, LOW);
   delay(1000);
   Serial.begin(115200);
-  
   Serial.printf(" ESP8266 Chip id = %08X\n", ESP.getChipId());
   
      if (!wifiManager.autoConnect("ConfigDomotics", "12345678")) {
@@ -37,6 +37,7 @@ void setup() {
 }
 String response;
 void loop(){
+  client.setHeader("X-Secret-Token: ffab1234abab");
   response = "";
   int statusCode = client.get(path, &response);
   Serial.print("Status code from server: ");
